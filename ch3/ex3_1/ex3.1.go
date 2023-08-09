37761c6688ac78a340ac9e91604eb2ce4f79f424
Cole-SJ <odon39126@gmail.com>
2023-08-09 23:16:00 +0900

    chapter3 studied

---
+++ ch3/ex3_1/ex3.1.go
@@ -0,0 +1,8 @@
+package main // package main은 프로그램의 엔트리포인트가 있는 패키지로 main 패키지 안에는 main 함수가 무조건 존재해야합니다.
+
+import "fmt" //표준 입출력을 다루는 내장 패키지
+
+// main 함수
+func main() {
+	fmt.Println("Hello Go World")
+}
