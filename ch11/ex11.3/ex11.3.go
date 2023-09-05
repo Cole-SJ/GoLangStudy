c27a5938cbedeb3cc2e0ab57c2d16863e7981a74
Cole-SJ <odon39126@gmail.com>
2023-09-06 01:09:40 +0900

    chapter11 source code

---
+++ ch11/ex11.3/ex11.3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println("안녕하세요")
+		var number int
+
+		_, err := fmt.Scanln(&number)
+
+		if err != nil {
+			fmt.Println("숫자를 입력하세요")
+
+			stdin.ReadString('\n')
+			continue
+		}
+
+		fmt.Printf("입력하신 숫자는 %d 입니다.\n", number)
+
+		if number%2 == 0 {
+			break
+		}
+	}
+
+	fmt.Println("for문이 종료되었습니다.")
+}
---
+++ ch11/ex11.6/ex11.6.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	dan := 2
+	b := 1
+	for true {
+		for true {
+			fmt.Printf("%d * %d = %d\n", dan, b, dan*b)
+			b++
+
+			if b == 10 {
+				break
+			}
+		}
+		b = 1
+		dan++
+
+		if dan == 10 {
+			break
+		}
+	}
+
+	fmt.Println("for문이 종료되었습니다")
+}
