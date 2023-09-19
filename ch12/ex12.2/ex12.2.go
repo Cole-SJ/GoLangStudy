1dd4d32534286a4143570351e0bd4c145dab50e1
Cole-SJ <odon39126@gmail.com>
2023-09-20 02:17:35 +0900

    chapter 12 source code

---
+++ ch12/ex12.2/ex12.2.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+const Y int = 3
+
+func main() {
+	//x := 5
+
+	//a := [x]int{1, 2, 3, 4, 5}
+	b := [Y]int{1, 2, 30}
+	var c [10]int
+	var d [4]string = [4]string{"adf"}
+
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+}
